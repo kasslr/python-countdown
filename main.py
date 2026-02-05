4f8e60661258d95d21c3dd2cf6adc95f24c622ff
kasslr <kass.stars+git@proton.me>
2026-02-05 15:21:29 +0100

    added h m s and made it clear the terminal

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import datetime
 import output
+import time
+import os
 
-today = datetime.datetime.now()
-arrival = datetime.datetime(2026,3,10)
-
-timeRemaining = arrival - today
+arrival = datetime.datetime(2026,3,10,0,0,0)
 
 def format_timedelta(td):
     total = td.days
     weeks, days = divmod(td.days, 7)
+    seconds = td.seconds
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
 
-    return f'{weeks} weeks and {days} days remaining'
+    return f'{weeks} weeks {days} days {hours}h {minutes}m {seconds}s remaining'
 
-output.generateOutput(format_timedelta(timeRemaining))
+while(True):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    today = datetime.datetime.now()
+    timeRemaining = arrival - today
+
+    output.generateOutput(format_timedelta(timeRemaining))
+    
+    time.sleep(1)
